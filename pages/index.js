bd4161d56cd9bdf27e1238d4c819ef578cb223c7
sanjaythavi <sanjay.thavi.india@gmail.com>
2025-03-14 20:44:21 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -11,7 +11,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <Header title="Welcome to my app!" />
+        <Header title="Welcome to SanSoft ERP Demo " />
         <p className="description">
           Click here to start the server
         </p>
